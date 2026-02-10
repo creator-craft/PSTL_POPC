c1787cdb91e32e781fca11eb3355d2a46c19dffd
creator-craft <crafteur903@gmail.com>
2026-02-10 15:46:23 +0100

    function: SURE

--- src/POP/FRAMEADV.c
+++ src/POP/FRAMEADV.c
@@ -11,11 +11,70 @@ void SURE() {
 
   rowno = 2;
 
-  Dy = BlockBot[rowno];
+  do { // row
+    Dy = BlockBot[rowno + 1];
 
-  Ay = Dy - 3; // - 1 ?? (carry)
+    Ay = Dy - 3 - 1; // -1 with carry
 
-  // Line 70
+    yindex = rowno * 10;
+
+    PRECED = PREV[rowno]; // prev_object
+    spreced = sprev[rowno]; // prev_state
+
+    getbelow();
+
+    colno = 0;
+    do { // loop
+      blockxco = XC0 = colno << 2; // X-coord for A-section
+
+      objid = getobjid(); // yindex as argument
+
+      RedBlockSure();
+
+      PRECED = objid;
+      spreced = state;
+
+      yindex ++;
+    } while (++colno <= 10);
+
+  } while (rowno--);
+  // rowno ++; // Not in original file (added to make a better loop)
+
+  // NOW: draw bottom row of screen above
+
+  rowno = 2;
+  Dy = 2;
+  Ay = -1;
+
+  yindex = rowno * 10;
+
+  PRECED = 0;
+  push(scrnBelow);
+  push(scrnBelowL);
+
+  scrnBelow = SCRNUM;
+  scrnBelowL = scrnLeft;
+
+  getbelow();
+  calcblue(scrnAbove);
+
+  colno = 0;
+  do { // dloop
+    blockxco = CXO = colno << 2;
+
+    if (scrnAbove || floor == 0) // TODO ???
+      getobjid1(scrnAbove, yindex); // Supposed
+    
+    RedDSure();
+
+    PRECED = objid;
+    spreced = state;
+
+    yindex ++;
+  } while (++colno <= 10);
+
+  scrnBelowL = pop();
+  scrnBelow = pop();
 }
 
 void FAST();
