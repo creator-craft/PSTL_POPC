952c32c8eafe405afb011d1488b5df1c4b204b98
magmigale <firespectre.2000@gmail.com>
2026-02-20 12:20:54 +0100

    première implementation

--- include/POP_external/Level.h
+++ include/POP_external/Level.h
@@ -5,31 +5,40 @@
 
 /**
  * Structure of Level
- * BlueType and BlueSpec contains the type and the state of a block
- * LinkLok an LinkMap contains information about the interactive element of the map
- * Map contains the screen neighbour of each screen 
- * Character_Position contains the data the position of caracters,
+ * blue_type and blue_spec contains the type and the state of a block
+ * link_lok an link_map contains information about the interactive element of the map
+ * map contains the screen neighbour of each screen 
+ * character_position contains the data the position of caracters,
  * the first positions are dedicated to child
+ * 
  */
 typedef struct _level{
-    uint8_t *BlueType;
-    uint8_t *BlueSpec;
-    uint8_t *LinkLok;
-    uint8_t *LinkMap;
+    uint8_t *blue_type;
+    uint8_t *blue_spec;
+    uint8_t *link_lok;
+    uint8_t *link_map;
     struct {
-        uint8_t Left;
-        uint8_t Right;
-        uint8_t Up;
-        uint8_t Down;
-    } *Map;
-    uint8_t *Character_Position;
+        uint8_t left;
+        uint8_t right;
+        uint8_t up;
+        uint8_t down;
+    } *map;
+    uint8_t *character_position;
+    int c_p_length;
 } LEVEL;
 
 /**
- * Allocate the memory, and initialyze the data of this level
- * with the data contained in the file level_file
+ * Allocate the memory used for the structure LEVEL
  */
-LEVEL* createLevel(char *level_file);
+LEVEL* createLevel();
+
+/**
+ * initiate the attributes of the LEVEL structure with the data
+ * contained in the file level_file
+ * return 0 if the initialisation were well done
+ * 1 else
+ */
+int initiateLevel(LEVEL *level, char *level_file);
 
 /**
  * Free the memory use by the LEVEL level
--- src/POP_external/Level.c
+++ src/POP_external/Level.c
@@ -1,24 +1,55 @@
 #include "POP_external/Level.h"
 #include <stdint.h>
+#include <stdio.h>
 
 LEVEL *createLevel(char *level_file){
     LEVEL *level_res = malloc(sizeof(LEVEL));
     if (level_res == NULL){return NULL;}
-    level_res->Map = malloc(24 * sizeof(level_res->Map));
+    level_res->map = malloc(24 * sizeof(*(level_res->map)));
     uint8_t *blue_type = malloc(720);
     uint8_t *blue_spec = malloc(720);
     uint8_t *linklok = malloc(256);
     uint8_t *linkmap = malloc(256);
     uint8_t *character_position = malloc(20);
-    int err = (level_res->Map == NULL);
+    int err = (level_res->map == NULL);
     err = err || !blue_type || !blue_spec;
     err = err || !linklok || !linkmap;
     err = err || !character_position;
     if (err) {return NULL;}
-    level_res->BlueSpec = blue_spec;
-    level_res->BlueType = blue_type;
-    level_res->Character_Position = character_position;
-    level_res->LinkLok = linklok;
-    level_res->LinkMap = linkmap;
+    level_res->blue_spec = blue_spec;
+    level_res->blue_type = blue_type;
+    level_res->character_position = character_position;
+    level_res->link_lok = linklok;
+    level_res->link_map = linkmap;
     return level_res;
+}
+
+int initiateLevel(LEVEL *level, char *level_file){
+    FILE *f = fopen(level_file, 'r');
+    if (f == NULL){return 1;}
+    size_t s;
+    s = fread(level->blue_type, 1, 720, f);
+    if (s < 720){return 1;}
+    s = fread(level->blue_spec, 1, 720, f);
+    if (s < 720){return 1;}
+    s = fread(level->link_lok, 1, 256, f);
+    if (s < 256){return 1;}
+    s = fread(level->link_map, 1, 256, f);
+    if (s < 256){return 1;}
+    s = fread(level->map, 1, 96, f);
+    if (s < 96){return 1;}
+    s = fread(level->character_position, 1, 20, f);
+    level->c_p_length = s;
+    return 0;
+}
+
+void destroyLevel(LEVEL *level){
+    free(level->blue_spec);
+    free(level->blue_type);
+    free(level->character_position);
+    free(level->link_lok);
+    free(level->link_lok);
+    free(level->link_map);
+    free(level->map);
+    free(level);
 }
