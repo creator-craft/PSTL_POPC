aa4f783fd61f971a124b4a2a908642233216dd2d
creator-craft <crafteur903@gmail.com>
2026-05-04 15:44:25 +0200

    I forgot

---
+++ src/POP_SDL/hires.c
@@ -0,0 +1,24 @@
+
+void fastlay(int img, int x, int y, int op) {
+  // if (op == STA) {
+  //   fastlaySTA(img, x, y);
+  //   return;
+  // }
+  // op == ORA
+}
+
+void fastlaySTA(int img, int x, int y) {
+  
+}
+
+
+// TODO
+void layrsave() {}
+void addpeel() {}
+void lay() {}
+void fastmask() {}
+void cls() {}
+void zeropeel() {}
+void drawwipe() {}
+void drawmsg() {}
+void sngpeel() {}
